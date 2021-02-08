d145f307baf6f54c5ffb1e605c13f396c5d82daa
xkevio <xkeviota@gmail.com>
2021-02-08 06:16:13 +0100

    added a variant that works with a class for each instance

---
+++ oop variant/brainfuck.h
@@ -0,0 +1,22 @@
+#include <string>
+#include <deque>
+class Brainfuck {
+
+    public:
+        Brainfuck(const std::string &input);
+        void execute(std::string &output, bool ascii);
+    private:
+        std::deque<unsigned char> cells;
+        std::string input;
+        bool ascii;
+
+        int current_index;
+        int current_char;
+
+        void remove_spaces();
+        void remove_new_lines();
+        void find_matching_end_bracket();
+        void find_matching_start_bracket();
+
+        void op_codes(const char &cur, std::string &output);
+};
---
+++ oop variant/main.cpp
@@ -0,0 +1,20 @@
+#include "brainfuck.h"
+
+#include <iostream>
+#include <string>
+
+int main(int, char**) {
+
+    std::string brainfuck_code;
+    std::string output_w_ascii;
+    
+    std::cout << "Enter your brainfuck code: " << std::endl;
+    std::cin >> brainfuck_code;
+
+    Brainfuck bf1(brainfuck_code);
+    bf1.execute(output_w_ascii, true);
+
+    std::cout << output_w_ascii << std::endl;
+
+    return EXIT_SUCCESS;
+}
